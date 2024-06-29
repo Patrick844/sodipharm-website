929d5ade9c8d932ce12e0d9912ea4b0c76b7a441
Patrick Saade <patricksaade20@hotmail.com>
2024-06-29 23:35:36 +0000

    r

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-import dotenv from "dotenv";
-dotenv.config();
-const nextConfig = {
-  env: {
-    DB_URI: process.env.DB_URI,
-    DB_CA: process.env.DB_CA,
-  },
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
+export default nextConfig;
