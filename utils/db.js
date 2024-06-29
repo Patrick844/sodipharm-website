a0ab74b488c16edc0558e039d2d3fb48d78072f5
Patrick Saade <patricksaade20@hotmail.com>
2024-06-29 19:59:12 +0000

    db_setup fix

--- utils/db.js
+++ utils/db.js
@@ -3,13 +3,17 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+console.log(process.env.DB_SSL_CA);
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED === "true",
+    ca: process.env.DB_SSL_CA,
+  },
 });
 
 export default pool;
