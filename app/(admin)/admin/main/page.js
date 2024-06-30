a26797c2b951e6660a3302396b7ee2e6d935a733
Patrick Saade <patricksaade20@hotmail.com>
2024-06-30 17:34:25 +0000

    design

--- app/(admin)/admin/main/page.js
+++ app/(admin)/admin/main/page.js
@@ -4,14 +4,18 @@ export default function AdminPage() {
   return (
     <div className="w-screen h-screen inset-0 flex items-center justify-center">
       <div className=" w-screen h-screen bg-gray-200 p-4 rounded-lg flex flex-col sm:flex-row items-center justify-evenly">
-        <div className="flex flex-col w-64 h-64 border-2 border-solid border-black justify-between pb-3">
-          <div className="text-lg text-center text-white bg-black">Article</div>
+        <div className="flex flex-col w-64 h-64 sm:w-80 sm:h-80 border-2 border-solid border-black justify-between pb-3">
+          <div className="text-2xl font-bold py-3 text-center text-white bg-black">
+            Article
+          </div>
           <ButtonAdmin text="Ajouter" url="/admin/article/add" />
           <ButtonAdmin text="Modifier/Suprimer" url="/admin/article/update" />
         </div>
 
-        <div className="flex flex-col w-64 h-64 border-2 border-solid border-black justify-between pb-3">
-          <div className="text-lg text-center text-white bg-black">News</div>
+        <div className="flex flex-col w-64 h-64 sm:w-80 sm:h-80 border-2 border-solid border-black justify-between pb-3">
+          <div className="text-2xl font-bold py-3  text-center text-white bg-black">
+            News
+          </div>
           <ButtonAdmin text="Ajouter" url="/admin/news/add" />
           <ButtonAdmin text="Modifier/Suprimer" url="/admin/news/update" />
         </div>
--- components/ButtonAdmin.js
+++ components/ButtonAdmin.js
@@ -2,7 +2,7 @@ function ButtonAdmin(props) {
   return (
     <a
       href={props.url}
-      className={`mx-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}
+      className={`mx-2 bg-blue-500 hover:bg-blue-700 text-white text-center sm:text-xl  font-bold py-4 px-4 rounded`}
     >
       {props.text}
     </a>
