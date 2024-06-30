233a0c5a40b9401cdaeafb74ac6df597abcb3075
Patrick Saade <patricksaade20@hotmail.com>
2024-06-30 11:30:14 +0000

    re

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -11,13 +11,6 @@ export async function POST(req, res) {
     // ]);
     console.log("login");
 
-    try {
-      await pool_uri.connect();
-      console.log("Connected to PostgreSQL database on Aiven");
-      // Perform database operations here
-    } catch (err) {
-      console.error("Error connecting to PostgreSQL:", err.message);
-    }
     const result = await pool_uri.query(
       "SELECT * FROM users WHERE username = $1",
       [username]
@@ -26,7 +19,14 @@ export async function POST(req, res) {
     if (result.rows.length === 0) {
       return NextResponse.json(
         { message: "Invalid username or password" },
-        { status: 401 }
+        { status: 401 },
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          },
+        }
       );
     }
 
@@ -37,17 +37,40 @@ export async function POST(req, res) {
     if (!isValidPassword) {
       return NextResponse.json(
         { message: "Invalid username or password" },
-        { status: 401 }
+        { status: 401 },
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          },
+        }
       );
     }
 
     // Successful login
-    return NextResponse.json({ message: "Login successful", success: true });
+    return NextResponse.json(
+      { message: "Login successful", success: true },
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+      }
+    );
   } catch (err) {
     console.error(err);
     return NextResponse.json(
       { message: "Internal server error" },
-      { status: 500 }
+      { status: 500 },
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+      }
     );
   }
 }
